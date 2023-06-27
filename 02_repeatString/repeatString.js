83a8df4783044bb25297d26e853c596c62ee8bfa
Patrick Thesken <patrick@student.dev-island.org>
2023-06-26 18:17:23 -1000

    fixed code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
 
+  let resultStr = "";
+
+  for (let i = 0; i < times; i++) {
+    resultStr += str;
+  }
+
+  return resultStr;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (paint) {
+  return paint.split("").reverse().join("");
 };
-
+console.log (reverseString ("paint"))
 // Do not edit below this line
 module.exports = reverseString;

73849d911feade4cfaacef0dc89647e35ef5513e
Patrick Thesken <patrick@student.dev-island.org>
2023-06-28 21:57:45 -1000

    fixed up sumAll, removeFromArray (waiting for partner to send the code we did together), reverseString, repeatString, helloWorld

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  let start = 0,
+    end = 0;
+  // making sure start is lower number end is higher
+  if (num1 < num2) {
+    start = num1;
+    end = num2;
+  } else {
+    start = num2;
+    end = num1;
+  }
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum = sum + i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

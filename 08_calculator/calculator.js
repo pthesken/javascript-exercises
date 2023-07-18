10857d4a16891daddb931f67415be608b9007002
Patrick Thesken <patrick@student.dev-island.org>
2023-07-17 21:05:26 -1000

    completed remeining codes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  let result = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    result += numArray[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+  let product = 1;
+  for (let i = 0; i < args.length; i++) {
+    product *= args[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+  if (count < 0) return "OOPS";
+  const fibPart = [0, 1];
+  for (let index = 1; index < count; index++) {
+    fibPart.push(fibPart[index] + fibPart[index - 1]);
+  }
+  return fibPart[count];
 };
 
 // Do not edit below this line
